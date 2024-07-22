a5f02949a6372db0bbfbe66e7c2533caa047e018
Aelto <Aelto@users.noreply.github.com>
2024-07-22 17:38:49 +0200

    update deps

--- scripts/make-redkit-depot.bat
+++ scripts/make-redkit-depot.bat
@@ -20,21 +20,8 @@ XCOPY "%modpath%\tw3-shared-utils\%depname%\" "%modpath%\deps\mods\%depname%\" /
 set depname=mod_sharedutils_oneliners
 XCOPY "%modpath%\tw3-shared-utils\%depname%\" "%modpath%\deps\mods\%depname%\" /e /s /y
 
-set depname=mod_sharedutils_storage
-XCOPY "%modpath%\tw3-shared-utils\%depname%\" "%modpath%\deps\mods\%depname%\" /e /s /y
-
-::
-::
-::
-:: generate the merges
-cd "%modpath%/deps"
-tw3-cahirp build --out "%modpath%\deps\scripts.cahirp" --clean --without-mods
-
 :: copy the vanilla scripts into the scripts
 XCOPY "%modpath%\deps\content\content0\scripts\" "%modpath%\deps\scripts\" /e /s /y
-
-:: copy the merged scripts into the scripts
-XCOPY "%modpath%\deps\scripts.cahirp\" "%modpath%\deps\scripts\" /e /s /y
 
 ::
 ::
@@ -50,9 +37,6 @@ set depname=mod_sharedutils_dialogChoices
 XCOPY "%modpath%\tw3-shared-utils\%depname%\content\scripts\local\" "%modpath%\deps\scripts\local\" /e /s /y
 
 set depname=mod_sharedutils_oneliners
-XCOPY "%modpath%\tw3-shared-utils\%depname%\content\scripts\local\" "%modpath%\deps\scripts\local\" /e /s /y
-
-set depname=mod_sharedutils_storage
 XCOPY "%modpath%\tw3-shared-utils\%depname%\content\scripts\local\" "%modpath%\deps\scripts\local\" /e /s /y
 
 :: copy the generated scripts into the depot
