90a2a6522736d89e39e7c11a0bbaaf439efd3bca
Aelto <Aelto@users.noreply.github.com>
2024-07-28 00:17:17 +0200

    Update release.bat

--- scripts/release.bat
+++ scripts/release.bat
@@ -16,9 +16,21 @@ XCOPY "%modpath%\dist\%depname%\" "%modpath%\release\mods\%depname%\" /e /s /y
 set depname=modWildMonsterHunt
 XCOPY "%modpath%\strings" "%modpath%\release\mods\%depname%\content\" /e /s /y
 
-:: copy the sharedutils dependencies
-set depname=mod_sharedutils_helpers
-XCOPY "%modpath%\tw3-shared-utils\%depname%\" "%modpath%\release\mods\%depname%\" /e /s /y
+@REM :: copy the sharedutils dependencies
+@REM set depname=mod_sharedutils_helpers
+@REM XCOPY "%modpath%\tw3-shared-utils\%depname%\" "%modpath%\release\mods\%depname%\" /e /s /y
+
+@REM set depname=mod_sharedutils_glossary
+@REM XCOPY "%modpath%\tw3-shared-utils\%depname%\" "%modpath%\release\mods\%depname%\" /e /s /y
+
+@REM set depname=mod_sharedutils_dialogChoices
+@REM XCOPY "%modpath%\tw3-shared-utils\%depname%\" "%modpath%\release\mods\%depname%\" /e /s /y
+
+@REM set depname=mod_sharedutils_damagemodifiers
+@REM XCOPY "%modpath%\tw3-shared-utils\%depname%\" "%modpath%\release\mods\%depname%\" /e /s /y
+
+@REM set depname=mod_sharedutils_oneliners
+@REM XCOPY "%modpath%\tw3-shared-utils\%depname%\" "%modpath%\release\mods\%depname%\" /e /s /y
 
 :: don't need a menu at the moment
 @REM mkdir "%modpath%\release\bin\config\r4game\user_config_matrix\pc\"
