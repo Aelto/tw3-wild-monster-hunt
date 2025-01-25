ad758e91b5e408ced3fa336f8c36c3238fa8506a
Aelto <Aelto@users.noreply.github.com>
2025-01-25 13:36:43 +0100

    wip:

--- scripts/install-workspace.bat
+++ scripts/install-workspace.bat
@@ -10,3 +10,7 @@ rmdir "%modPath%\redkit\wild_monster_hunt\workspace\scripts\local\wss" /s /q
 echo Copying release into redkit workspace
 XCOPY "%modPath%\release\mods\%modName%\content\scripts" "%modPath%\redkit\wild_monster_hunt\workspace\scripts\local\wss\" /e /s /y
 
+if "%1"=="-game" (
+XCOPY "%modPath%\redkit\wild_monster_hunt\workspace\scripts\local\wss" "%gamePath%\mods\modwild_monster_hunt\content\scripts\local\wss\" /e /s /y
+XCOPY "%modPath%\redkit\wild_monster_hunt\workspace\scripts\local\wmhredkit" "%gamePath%\mods\modwild_monster_hunt\content\scripts\local\wmhredkit\" /e /s /y
+)
